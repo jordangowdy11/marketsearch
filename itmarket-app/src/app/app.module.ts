0367fa857856b2afab3a15ed32d6ffa23b1135a7
61082EleanorBound <44974866+61082EleanorBound@users.noreply.github.com>
2019-01-25 10:41:37 +0000

    Ellie pushing new components

--- itmarket-app/src/app/app.module.ts
+++ itmarket-app/src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HtmlComponent } from './html/html.component';
 import { SqlComponent } from './sql/sql.component';
+import { JavaScriptComponent } from './java-script/java-script.component';
+import { CSharpComponent } from './csharp/csharp.component';
+import { MicrosoftComponent } from './microsoft/microsoft.component';
+import { JavaComponent } from './java/java.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HtmlComponent,
-    SqlComponent
+    SqlComponent,
+    JavaScriptComponent,
+    CSharpComponent,
+    MicrosoftComponent,
+    JavaComponent
   ],
   imports: [
     BrowserModule,
---
+++ itmarket-app/src/app/microsoft.ts
@@ -0,0 +1,2 @@
+export interface Microsoft {
+}
