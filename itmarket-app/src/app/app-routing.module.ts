e436648541c2d9b50a0b5a9b0feeee8eb4aac912
61082EleanorBound <44974866+61082EleanorBound@users.noreply.github.com>
2019-01-25 11:51:49 +0000

    pushing all linked things

--- itmarket-app/src/app/app-routing.module.ts
+++ itmarket-app/src/app/app-routing.module.ts
@@ -12,9 +12,19 @@ import { HtmlComponent } from './html/html.component';
 import { ScrumComponent } from './scrum/scrum.component';
 import { DevopsComponent } from './devops/devops.component';
 import { AwsComponent } from './aws/aws.component';
+import { PermanantpythonComponent } from './permanantpython/permanantpython.component';
+import { PermanantsqlComponent } from './permanantsql/permanantsql.component';
+import { PermanantjavascriptComponent } from './permanantjavascript/permanantjavascript.component';
+import { PermanantcsharpComponent } from './permanantcsharp/permanantcsharp.component';
+import { PermanantmicrosoftComponent } from './permanantmicrosoft/permanantmicrosoft.component';
+import { PermanantjavaComponent } from './permanantjava/permanantjava.component';
+import { PermananthtmlComponent } from './permananthtml/permananthtml.component';
+import { PermanantscrumComponent } from './permanantscrum/permanantscrum.component';
+import { PermanantdevopsComponent } from './permanantdevops/permanantdevops.component';
+import { PermanantawsComponent } from './permanantaws/permanantaws.component';
 
 const routes: Routes = [
-  {path:'permanant', component:PermanentComponent},
+  {path:'permanent', component:PermanentComponent},
   {path:'contract', component:ContractedComponent},
   {path:'contract/python',component:PythonComponent},
   {path:'contract/sql',component:SqlComponent},
@@ -26,6 +36,16 @@ const routes: Routes = [
   {path:'contract/scrum',component:ScrumComponent},
   {path:'contract/devops',component:DevopsComponent},
   {path:'contract/aws',component:AwsComponent},
+  {path:'permanent/python',component:PermanantpythonComponent},
+  {path:'permanent/sql',component:PermanantsqlComponent},
+  {path:'permanent/javascript',component:PermanantjavascriptComponent},
+  {path:'permanent/csharp',component:PermanantcsharpComponent},
+  {path:'permanent/microsoft',component:PermanantmicrosoftComponent},
+  {path:'permanent/java',component:PermanantjavaComponent},
+  {path:'permanent/html',component:PermananthtmlComponent},
+  {path:'permanent/scrum',component:PermanantscrumComponent},
+  {path:'permanent/devops',component:PermanantdevopsComponent},
+  {path:'permanent/aws',component:PermanantawsComponent}
 ];
 
 @NgModule({
