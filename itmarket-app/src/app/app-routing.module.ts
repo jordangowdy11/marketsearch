ccf7dfcae0f4c79793e5775a4dcb6dc0d2b3f90c
61082EleanorBound <44974866+61082EleanorBound@users.noreply.github.com>
2019-01-25 11:21:11 +0000

    sorting routing for each component

--- itmarket-app/src/app/app-routing.module.ts
+++ itmarket-app/src/app/app-routing.module.ts
@@ -2,10 +2,30 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PermanentComponent } from './permanent/permanent.component';
 import { ContractedComponent } from './contracted/contracted.component';
+import { PythonComponent } from './python/python.component';
+import { SqlComponent } from './sql/sql.component';
+import { JavaScriptComponent } from './java-script/java-script.component';
+import { CSharpComponent } from './csharp/csharp.component';
+import { MicrosoftComponent } from './microsoft/microsoft.component';
+import { JavaComponent } from './java/java.component';
+import { HtmlComponent } from './html/html.component';
+import { ScrumComponent } from './scrum/scrum.component';
+import { DevopsComponent } from './devops/devops.component';
+import { AwsComponent } from './aws/aws.component';
 
 const routes: Routes = [
   {path:'permanant', component:PermanentComponent},
   {path:'contract', component:ContractedComponent},
+  {path:'contract/python',component:PythonComponent},
+  {path:'contract/sql',component:SqlComponent},
+  {path:'contract/javascript',component:JavaScriptComponent},
+  {path:'contract/csharp',component:CSharpComponent},
+  {path:'contract/microsoft',component:MicrosoftComponent},
+  {path:'contract/java',component:JavaComponent},
+  {path:'contract/html',component:HtmlComponent},
+  {path:'contract/scrum',component:ScrumComponent},
+  {path:'contract/devops',component:DevopsComponent},
+  {path:'contract/aws',component:AwsComponent},
 ];
 
 @NgModule({
