d2b58ba0b8596520cf15a79ed0380183aae2fd13
jordangowdy11 <jordan.gowdy@mastek.com>
2019-01-25 10:19:13 +0000

    html component

--- itmarket-app/src/app/app.module.ts
+++ itmarket-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HtmlComponent } from './html/html.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HtmlComponent
   ],
   imports: [
     BrowserModule,
