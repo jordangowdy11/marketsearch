7128404d2da0c58342c708af001445929674a261
61082EleanorBound <44974866+61082EleanorBound@users.noreply.github.com>
2019-01-25 14:16:42 +0000

    pushing service.js file

--- itmarket-app/src/app/app.component.ts
+++ itmarket-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Statistics } from './stats';
 
 
 @Component({
@@ -8,4 +9,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'itmarket-app';
+
+  stats: Statistics[]
 }
---
+++ itmarket-app/src/app/stats.ts
@@ -0,0 +1,5 @@
+export class Statistics{
+    2019:number;
+    2018:number;
+    2017:number;
+}
