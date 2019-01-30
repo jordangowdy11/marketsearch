cc1ffa8c642af8a17a0c96c61e38e6dc12fada8b
61082EleanorBound <44974866+61082EleanorBound@users.noreply.github.com>
2019-01-30 15:45:11 +0000

    Changes to angular GUI

--- itmarket-app/src/app/headings.ts
+++ itmarket-app/src/app/headings.ts
@@ -1,2 +1,15 @@
 export interface Headings {
+    h1:string;
+    h2:string;
+    h3:string;
+    h4:string;
+    h5:string;
+    h6:string;
+    h7:string;
+    h8:string;
+    h9:string;
+    h10:string;
+    h11:string;
+    h12:string;
+
 }
--- itmarket-app/src/app/permanent.ts
+++ itmarket-app/src/app/permanent.ts
@@ -1,4 +1,7 @@
+import { Headings } from "./headings";
+
 export class Permanent {
+    headings: Array<Headings>;
     thisyeardata: string;
     lastyeardata: string;
     twoyearagodata: string;
