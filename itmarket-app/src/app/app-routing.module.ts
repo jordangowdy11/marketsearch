3103b18bc5792eea1e05b4c74e1984c4b83d2ac1
61082EleanorBound <44974866+61082EleanorBound@users.noreply.github.com>
2019-01-30 16:04:49 +0000

    merge changes

--- itmarket-app/src/app/app-routing.module.ts
+++ itmarket-app/src/app/app-routing.module.ts
@@ -8,20 +8,17 @@ import { PythonComponent } from './python/python.component';
 import { SqlComponent } from './sql/sql.component';
 import { JavaScriptComponent } from './java-script/java-script.component';
 import { CSharpComponent } from './csharp/csharp.component';
-
 import { MicrosoftComponent } from './microsoft/microsoft.component';
 import { JavaComponent } from './java/java.component';
 import { HtmlComponent } from './html/html.component';
 import { ScrumComponent } from './scrum/scrum.component';
 import { DevopsComponent } from './devops/devops.component';
 import { AwsComponent } from './aws/aws.component';
-//we are only doing permanent at the mo!
 import { PermanantpythonComponent } from './permanantpython/permanantpython.component';
 import { PermanantsqlComponent } from './permanantsql/permanantsql.component';
 import { PermanantjavascriptComponent } from './permanantjavascript/permanantjavascript.component';
 import { PermanantcsharpComponent } from './permanantcsharp/permanantcsharp.component';
 import { PermanantmicrosoftComponent } from './permanantmicrosoft/permanantmicrosoft.component';
-//j do form here <perm-language>
 import { PermanantjavaComponent } from './permanantjava/permanantjava.component';
 import { PermananthtmlComponent } from './permananthtml/permananthtml.component';
 import { PermanantscrumComponent } from './permanantscrum/permanantscrum.component';
@@ -31,26 +28,26 @@ import { PermanantawsComponent } from './permanantaws/permanantaws.component';
 const routes: Routes = [
   {path:'permanent', component:PermanentComponent, data: {animation: 'PermPage'}},
   {path:'contract', component:ContractedComponent, data: {animation: 'ContractPage'}},
-  {path:'contract/python',component:PythonComponent, data: {animation: 'ContPy'}},
-  {path:'contract/sql',component:SqlComponent, data: {animation: 'ContSQL'}},
-  {path:'contract/javascript',component:JavaScriptComponent, data: {animation: 'ContJS'}},
-  {path:'contract/csharp',component:CSharpComponent, data: {animation: 'ContCSharp'}},
-  {path:'contract/microsoft',component:MicrosoftComponent, data: {animation: 'ContMS'}},
-  {path:'contract/java',component:JavaComponent, data: {animation: 'ContJava'}},
-  {path:'contract/html',component:HtmlComponent, data: {animation: 'ContHtml'}},
-  {path:'contract/scrum',component:ScrumComponent, data: {animation: 'ContScrum'}},
-  {path:'contract/devops',component:DevopsComponent, data: {animation: 'ContDevops'}},
-  {path:'contract/aws',component:AwsComponent, data: {animation: 'ContAWS'}},
-  {path:'permanent/python',component:PermanantpythonComponent, data: {animation: 'PermPy'}},
-  {path:'permanent/sql',component:PermanantsqlComponent, data: {animation: 'PermSQL'}},
-  {path:'permanent/javascript',component:PermanantjavascriptComponent, data: {animation: 'PermJS'}},
-  {path:'permanent/csharp',component:PermanantcsharpComponent, data: {animation: 'PermCSharp'}},
-  {path:'permanent/microsoft',component:PermanantmicrosoftComponent, data: {animation: 'PermMS'}},
-  {path:'permanent/java',component:PermanantjavaComponent, data: {animation: 'PermJava'}},
-  {path:'permanent/html',component:PermananthtmlComponent, data: {animation: 'PermHtml'}},
-  {path:'permanent/scrum',component:PermanantscrumComponent, data: {animation: 'PermScrum'}},
-  {path:'permanent/devops',component:PermanantdevopsComponent, data: {animation: 'PermDevops'}},
-  {path:'permanent/aws',component:PermanantawsComponent, data: {animation: 'PermAWS'}}
+  {path:'contract/python',component:PythonComponent},
+  {path:'contract/sql',component:SqlComponent},
+  {path:'contract/javascript',component:JavaScriptComponent},
+  {path:'contract/csharp',component:CSharpComponent},
+  {path:'contract/microsoft',component:MicrosoftComponent},
+  {path:'contract/java',component:JavaComponent},
+  {path:'contract/html',component:HtmlComponent},
+  {path:'contract/scrum',component:ScrumComponent},
+  {path:'contract/devops',component:DevopsComponent},
+  {path:'contract/aws',component:AwsComponent},
+  {path:'permanent/python',component:PermanantpythonComponent},
+  {path:'permanent/sql',component:PermanantsqlComponent},
+  {path:'permanent/javascript',component:PermanantjavascriptComponent},
+  {path:'permanent/csharp',component:PermanantcsharpComponent},
+  {path:'permanent/microsoft',component:PermanantmicrosoftComponent},
+  {path:'permanent/java',component:PermanantjavaComponent},
+  {path:'permanent/html',component:PermananthtmlComponent},
+  {path:'permanent/scrum',component:PermanantscrumComponent},
+  {path:'permanent/devops',component:PermanantdevopsComponent},
+  {path:'permanent/aws',component:PermanantawsComponent}
 ];
 
 @NgModule({
