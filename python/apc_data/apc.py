5c4d79c9d58203876b736a9a03624f7d138b81f3
Oliver <44974906+ocgMastek@users.noreply.github.com>
2019-01-30 11:34:41 +0000

    fixed amount of data stored in csv files

--- python/apc_data/apc.py
+++ python/apc_data/apc.py
@@ -3,6 +3,8 @@ import requests
 import pprint
 from bs4 import BeautifulSoup
 import pandas as pd
+import os
+import datetime
 
 ##pprint formats terminal prints
 pp = pprint.PrettyPrinter()
@@ -11,8 +13,12 @@ pp = pprint.PrettyPrinter()
 crawler_topics = ('amazon aws', 'csharp', 'java', 'html', 'javascript',
                   'sql', 'scrum', 'python', 'microsoft', 'devops')
 
+# temp_topics = "microsoft"
+
 column_titles = ('thisyeardata', 'lastyeardata', 'twoyearagodata')
 list_of_data = []
+
+csv_folder = "stored_csv_"
 
 def get_new_data(crawler_topic):
     print(crawler_topic)
@@ -25,13 +31,34 @@ def get_new_data(crawler_topic):
     while x < 36:
         list_of_data.append(tr_data[x].text.strip())
         x += 1
-    csv_data = pd.np.array(list_of_data).reshape((len(list_of_data) // 3, 3))
-    print(csv_data)
-    pd.DataFrame(csv_data, columns=column_titles).to_csv("/csvdata/" + crawler_topic + ".csv", index=False)
+        if x == 36:
+            csv_data = pd.np.array(list_of_data).reshape((len(list_of_data) // 3, 3))
+            print(csv_data)
+            pd.DataFrame(csv_data, columns=column_titles).to_csv(crawler_topic + ".csv", index=False)
+            list_of_data.clear()
+
+
+    #create a directory using a string and current date converted to a string as the folder name
+    #this is to house all csv data, new files are created to house new data, all date organised
+def make_dir():
+    current_date = datetime.datetime.now()
+    date = str(current_date.day) + "-"+ str(current_date.month) + "-"+ str(current_date.year)
+    new_dir = csv_folder + date
+    if os.path.isdir(new_dir):
+        print("Exists")
+    else:
+        try:    
+            os.makedirs(new_dir)
+        except OSError as err:
+            print(err)
 
 if __name__ == "__main__":
+    # make_dir()
+
     for x in crawler_topics:
         get_new_data(x)
+    # get_new_data(temp_topics)
+
 
 #iterate all topics in the list, make get request, crawl table data, store data in files
 
