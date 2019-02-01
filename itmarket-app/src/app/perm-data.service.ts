fadc6f74ab81ba85b561f16d878cc70f094c6389
Oliver <44974906+ocgMastek@users.noreply.github.com>
2019-02-01 09:41:52 +0000

    removed commented functions inside service files and express server

--- itmarket-app/src/app/perm-data.service.ts
+++ itmarket-app/src/app/perm-data.service.ts
@@ -9,69 +9,13 @@ import { HttpClient } from '../../node_modules/@angular/common/http';
 export class PermDataService {
 
   rootURL: string;
-  pythonURL:string;
-  javaURL:string;
-  htmlURL:string;
-  sqlURL:string;
-  javascriptURL:string;
-  csharpURL:string;
-  msURL:string;
-  scrumURL:string;
-  devopsURL:string;
-  awsURL:string;
-
-
 
   constructor(private httpsvc: HttpClient) {
     this.rootURL ="http://localhost:3400/language_data/mongoquery/"
    }
 
    //this is the base format for the data request, just replicate this for every language
-
    getPermDocumentData(getParam):Observable<Permanent[]>{
      return this.httpsvc.get<Permanent[]>(this.rootURL + getParam )
    }
-  //  getDocumentData():Observable<Permanent[]>{
-  //   //  return this.httpsvc.get<Permanent[]>(this.rootURL+"/javascript")
-  //   return this.httpsvc.get<Permanent[]>(this.rootURL)
-  // }
-
-  // getPythonData():Observable<Permanent[]>{
-  //   return this.httpsvc.get<Permanent[]>(this.pythonURL)
-  // }
-  
-  // getSqlData():Observable<Permanent[]>{
-  //   return this.httpsvc.get<Permanent[]>(this.sqlURL)
-  // }
-  
-  // getJavaData():Observable<Permanent[]>{
-  //   return this.httpsvc.get<Permanent[]>(this.javaURL)
-  // }
-  
-  // getHtmlData():Observable<Permanent[]>{
-  //   return this.httpsvc.get<Permanent[]>(this.htmlURL)
-  // }
-  
-  // getJavascriptData():Observable<Permanent[]>{
-  //   return this.httpsvc.get<Permanent[]>(this.javascriptURL)
-  // }
-  // getCsharpData():Observable<Permanent[]>{
-  //   return this.httpsvc.get<Permanent[]>(this.csharpURL)
-  // }
-
-  // getMicrosoftData():Observable<Permanent[]>{
-  //   return this.httpsvc.get<Permanent[]>(this.msURL)
-  // }
-
-  // getScrumData():Observable<Permanent[]>{
-  //   return this.httpsvc.get<Permanent[]>(this.scrumURL)
-  // }
-
-  // getDevopsData():Observable<Permanent[]>{
-  //   return this.httpsvc.get<Permanent[]>(this.devopsURL)
-  // }
-
-  // getAwsData():Observable<Permanent[]>{
-  //   return this.httpsvc.get<Permanent[]>(this.awsURL)
-  // }
 }
